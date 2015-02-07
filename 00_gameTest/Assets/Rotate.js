bd9b18af3fb0e3a28148e343c9c111c67f1a4c08
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-02-07 14:10:04 +0900

    ketch a study‐aid book to 30p

---
+++ 00_gameTest/Assets/Rotate.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	 transform.Rotate( 0,1,0 );
+	 transform.localScale.x =   Mathf.Sin( Time.time * 10.0 ) + 1.5;
+}
---
+++ 00_gameTest/Assets/Translate.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+public var x : float;
+public var y : float;
+public var z : float;
+
+function Update () {
+	 transform.Translate(0, 0, 1 * Time.deltaTime);
+}
