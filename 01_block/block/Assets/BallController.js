da4d6a7389e02638afe6e4e0150f727ba6197d22
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-07-04 22:47:20 +0900

    add game break the block

---
+++ 01_block/block/Assets/BallController.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var Speed = 20.0;
+function Start () {
+	rigidbody.AddForce(( transform.forward + transform.right ) * Speed, ForceMode.VelocityChange);
+}
+
+function Update () {
+
+}
