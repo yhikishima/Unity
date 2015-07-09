6a7ed2e72a7e06f4d46b42c76a207e462bf4181c
yhikishima <lallapalooza1120@gmail.com>
2015-07-09 09:08:39 +0900

    add sound and color

---
+++ 01_block/block/Assets/HitPlaySound.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+public var sound : AudioClip;
+function OnCollisionEnter(collision: Collision) {
+	AudioSource.PlayClipAtPoint(sound, transform.position);
+}
