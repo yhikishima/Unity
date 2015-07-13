e8f4078879b2c6aff8b225376effa799ec346ec4
yhikishima <lallapalooza1120@gmail.com>
2015-07-13 22:49:04 +0900

    add bear and human

---
+++ BazookaGame/BazookaGame/Assets/Scripts/GameControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+	public GameObject bear;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 9; i++) {
+			Instantiate(bear);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
