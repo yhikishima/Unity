f400bfc76ad065c54fc1a3752f99caf0f60f2e38
yhikishima <lallapalooza1120@gmail.com>
2015-07-20 18:21:40 +0900

    Add Coin Action

---
+++ 02_blockShot/Assets/Pusher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pusher : MonoBehaviour {
+	// default position
+	private Vector3 origin;
+
+	// Use this for initialization
+	void Start () {
+		// set default position
+		origin = rigidbody.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 offset = new Vector3 (0, 0, Mathf.Sin (Time.time));
+		rigidbody.MovePosition (origin + offset);
+	
+	}
+}
