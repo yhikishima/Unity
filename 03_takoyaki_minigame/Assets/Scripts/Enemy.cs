ebcbad45019defb68ffa60b30072a655e0754c84
yhikishima <lallapalooza1120@gmail.com>
2015-07-22 09:09:35 +0900

    Add Enemy Action And Scripts

---
+++ 03_takoyaki_minigame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : Token {
+
+	// Use this for initialization
+	void Start () {
+		float dir = Random.Range(0, 359);
+		float spd = 5;
+		SetVelocity (dir, spd);
+	
+	}
+
+	void Update() {
+		Vector2 min = GetWorldMin ();
+		Vector2 max = GetWorldMax ();
+
+		if (X < min.x || max.x < X) {
+			VX *= -1;
+			ClampScreen();
+		}
+		if (Y < min.y || max.y < Y) {
+			VY *= -1;
+			ClampScreen();
+		}
+	}
+}
