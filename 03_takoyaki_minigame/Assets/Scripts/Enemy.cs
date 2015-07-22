fee34e5af456577a7c673c54631ced6e77df4696
yhikishima <lallapalooza1120@gmail.com>
2015-07-23 08:30:52 +0900

    Add Click Event

--- 03_takoyaki_minigame/Assets/Scripts/Enemy.cs
+++ 03_takoyaki_minigame/Assets/Scripts/Enemy.cs
@@ -5,10 +5,11 @@ public class Enemy : Token {
 
 	// Use this for initialization
 	void Start () {
+		SetSize(SpriteWidth / 2, SpriteHeight / 2);
+
 		float dir = Random.Range(0, 359);
 		float spd = 5;
 		SetVelocity (dir, spd);
-	
 	}
 
 	void Update() {
@@ -23,5 +24,9 @@ public class Enemy : Token {
 			VY *= -1;
 			ClampScreen();
 		}
+	}
+
+	public void OnMouseDown() {
+		DestroyObj ();
 	}
 }
