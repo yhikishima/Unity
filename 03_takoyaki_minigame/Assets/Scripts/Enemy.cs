0cb0131852b41de87ad0a6bb6c48ff5639a6a078
yhikishima <lallapalooza1120@gmail.com>
2015-07-25 16:22:34 +0900

    Add Game Clear Action And View

--- 03_takoyaki_minigame/Assets/Scripts/Enemy.cs
+++ 03_takoyaki_minigame/Assets/Scripts/Enemy.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class Enemy : Token {
 
+	public static int Count = 0;
+
 	// Use this for initialization
 	void Start () {
+		Count++;
 		SetSize(SpriteWidth / 2, SpriteHeight / 2);
 
 		float dir = Random.Range(0, 359);
@@ -27,6 +30,7 @@ public class Enemy : Token {
 	}
 
 	public void OnMouseDown() {
+		Count--;
 
 		for (int i = 0; i < 32; i++) {
 			Particle.Add (X, Y);	
---
+++ 03_takoyaki_minigame/Assets/Scripts/GameMgr.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMgr : MonoBehaviour {
+	void OnGUI() {
+		if (Enemy.Count == 0) {
+			Util.SetFontSize (32);
+			Util.SetFontAlignment(TextAnchor.MiddleCenter);
+
+			float w = 128;
+			float h = 32;
+			float px = Screen.width / 2 - w /2;
+			float py = Screen.height /2 - h /2;
+
+			Util.GUILabel(px, py, w, h, "Game Clear!");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
