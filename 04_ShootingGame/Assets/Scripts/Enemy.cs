36607f8dcef3ab83edec7d7a3de39aacab948061
yhikishima <lallapalooza1120@gmail.com>
2015-07-28 09:13:10 +0900

    Add Enemy Action

---
+++ 04_ShootingGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	Spaceship spaceship;
+
+	// Use this for initialization
+	void Start () {
+		spaceship = GetComponent<Spaceship>();
+		spaceship.Move (transform.up * -1);
+	}
+
+}
