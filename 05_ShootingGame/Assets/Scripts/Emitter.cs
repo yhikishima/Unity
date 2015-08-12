b0e4af8be419b902b2ce78e937174a3e2f421299
yhikishima <lallapalooza1120@gmail.com>
2015-08-12 23:21:27 +0900

    End Seven Times Training

---
+++ 05_ShootingGame/Assets/Scripts/Emitter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Emitter : MonoBehaviour {
+
+	public GameObject[] waves;
+
+	private int currentWave;
+
+	IEnumerator Start() {
+		if (waves.Length == 0) {
+			yield break;	
+		}
+
+		while (true) {
+			GameObject wave = (GameObject)Instantiate (waves[currentWave], transform.position, Quaternion.identity);	
+
+			while(wave.transform.childCount != 0) {
+				yield return new WaitForEndOfFrame();
+			}
+			Destroy(wave);
+
+			if (waves.Length <= ++currentWave) {
+				currentWave = 0;
+			}
+		}
+	}
+
+}
