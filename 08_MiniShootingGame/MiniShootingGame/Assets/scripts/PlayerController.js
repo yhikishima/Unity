be50d3248f57f69d1621eeab7a3cebe1f445af98
yhikishima <lallapalooza1120@gmail.com>
2015-08-25 09:02:16 +0900

    Add JS

---
+++ 08_MiniShootingGame/MiniShootingGame/Assets/scripts/PlayerController.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var movingSpeed : float = 5.0;
+private var controller : CharacterController;
+private var velocity : Vector3;
+
+function Start () {
+	controller = GetComponent(CharacterController);
+
+}
+
+function Update () {
+	transform.position.y = 0;
+	velocity = Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	velocity *= movingSpeed;
+	
+	controller.Move(velocity * Time.deltaTime);
+}
--- 08_MiniShootingGame/README.md
+++ 08_MiniShootingGame/README.md
@@ -3,3 +3,5 @@ MiniShootingGame
 
 # 参考サイト
 [製作6時間！Unityで撃ちまくり系ミニゲームver2を作ったので手順を公開！](http://0310unity.hateblo.jp/entry/shoot_1)
+
+[Unity3Dで2Dのシューティングゲームを作ってみるチュートリアルその1](http://loumo.jp/wp/archive/20130213083341/)
