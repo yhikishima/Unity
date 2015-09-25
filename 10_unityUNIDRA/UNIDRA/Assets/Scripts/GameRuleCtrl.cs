4722bba6b054cf8a13cda6d4a2ea22c24f49ea63
yhikishima <lallapalooza1120@gmail.com>
2015-09-25 18:00:05 +0900

    End No.12 chapter

---
+++ 10_unityUNIDRA/UNIDRA/Assets/Scripts/GameRuleCtrl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameRuleCtrl : MonoBehaviour {
+	// 残り時間
+	public float timeRemaining = 5.0f * 60.0f;
+	// ゲームオーバーフラグ
+	public bool gameOver = false;
+	// ゲームクリア
+    public bool gameClear = false;
+
+	void Update()
+	{
+		timeRemaining -= Time.deltaTime;
+		// 残り時間が無くなったらゲームオーバー
+		if(timeRemaining<= 0.0f ){
+			GameOver();
+		}
+	}
+	
+	public void GameOver()
+	{
+		gameOver = true;
+        Debug.Log("GameOver");
+	}
+	public void GameClear()
+	{
+		gameClear = true;
+        Debug.Log("GameClear");
+    }
+}
