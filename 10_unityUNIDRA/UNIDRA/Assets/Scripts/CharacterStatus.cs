d39a246109b347b2334a6f27e5d469b3b225f4b9
yhikishima <lallapalooza1120@gmail.com>
2015-09-26 18:04:37 +0900

    Add Effect

--- 10_unityUNIDRA/UNIDRA/Assets/Scripts/CharacterStatus.cs
+++ 10_unityUNIDRA/UNIDRA/Assets/Scripts/CharacterStatus.cs
@@ -29,6 +29,9 @@ public class CharacterStatus : MonoBehaviour
     // 攻撃強化時間
     float powerBoostTime = 0.0f;
 
+    // 攻撃力強化エフェクト
+    ParticleSystem powerUpEffect;
+
     // アイテム取得
     public void GetItem(DropItem.ItemKind itemKind)
     {
@@ -36,6 +39,7 @@ public class CharacterStatus : MonoBehaviour
         {
             case DropItem.ItemKind.Attack:
                 powerBoostTime = 5.0f;
+                powerUpEffect.Play ();
                 break;
             case DropItem.ItemKind.Heal:
                 // MaxHPの半分回復
@@ -43,14 +47,30 @@ public class CharacterStatus : MonoBehaviour
                 break;
         }
     }
- 
+
+    void Start()
+    {
+        if (gameObject.tag == "Player")
+        {
+            powerUpEffect = transform.Find("PowerUpEffect").GetComponent<ParticleSystem>();
+        }
+    }
+
     void Update()
     {
-         powerBoost = false;
+        if (gameObject.tag != "Player")
+        {
+            return;
+        }
+        powerBoost = false;
         if (powerBoostTime > 0.0f)
         {
             powerBoost = true;
             powerBoostTime = Mathf.Max(powerBoostTime - Time.deltaTime, 0.0f);
+        }
+        else
+        {
+            powerUpEffect.Stop();
         }
     }
 
--- 10_unityUNIDRA/UNIDRA/Assets/Scripts/PlayerCtrl.cs
+++ 10_unityUNIDRA/UNIDRA/Assets/Scripts/PlayerCtrl.cs
@@ -10,7 +10,9 @@ public class PlayerCtrl : MonoBehaviour
 	InputManager inputManager;
 	public float attackRange = 1.5f;
 	GameRuleCtrl gameRuleCtrl;
-	
+	public GameObject hitEffect;
+	TargetCursor targetCursor;
+
 	// ステートの種類.
 	enum State
 	{
@@ -18,11 +20,14 @@ public class PlayerCtrl : MonoBehaviour
 		Attacking,
 		Died,
 	} ;
-	
+
 	State state = State.Walking;		// 現在のステート.
 	State nextState = State.Walking;	// 次のステート.
-	
-	
+
+	public AudioClip deathSeClip;
+	AudioSource deathSeAudio;
+
+
 	// Use this for initialization
 	void Start()
 	{
@@ -30,8 +35,15 @@ public class PlayerCtrl : MonoBehaviour
 		charaAnimation = GetComponent<CharaAnimation>();
 		inputManager = FindObjectOfType<InputManager>();
 		gameRuleCtrl = FindObjectOfType<GameRuleCtrl>();
+		targetCursor = FindObjectOfType<TargetCursor>();
+		targetCursor.SetPosition(transform.position);
+
+		// オーディオの初期化.
+		deathSeAudio = gameObject.AddComponent<AudioSource>();
+		deathSeAudio.loop = false;
+		deathSeAudio.clip = deathSeClip;
 	}
-	
+
 	// Update is called once per frame
 	void Update()
 	{
@@ -44,7 +56,7 @@ public class PlayerCtrl : MonoBehaviour
 			Attacking();
 			break;
 		}
-		
+
 		if (state != nextState)
 		{
 			state = nextState;
@@ -56,25 +68,25 @@ public class PlayerCtrl : MonoBehaviour
 			case State.Attacking:
 				AttackStart();
 				break;
-			case State.Died:
-				Died();
-				break;
-			}
+            case State.Died:
+                Died();
+                break;
+            }
 		}
 	}
-	
-	
+
+
 	// ステートを変更する.
 	void ChangeState(State nextState)
 	{
 		this.nextState = nextState;
 	}
-	
+
 	void WalkStart()
 	{
 		StateStartCommon();
 	}
-	
+
 	void Walking()
 	{
 		if (inputManager.Clicked())
@@ -86,7 +98,10 @@ public class PlayerCtrl : MonoBehaviour
 			{
 				// 地面がクリックされた.
 				if (hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+				{
 					SendMessage("SetDestination", hitInfo.point);
+					targetCursor.SetPosition(hitInfo.point);
+				}
 				// 敵がクリックされた.
 				if (hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("EnemyHit"))
 				{
@@ -98,44 +113,55 @@ public class PlayerCtrl : MonoBehaviour
 					{
 						// 攻撃.
 						attackTarget = hitInfo.collider.transform;
+						targetCursor.SetPosition(attackTarget.position);
 						ChangeState(State.Attacking);
 					}
 					else
+					{
 						SendMessage("SetDestination", hitInfo.point);
+						targetCursor.SetPosition(hitInfo.point);
+					}
 				}
 			}
 		}
 	}
-	
+
 	// 攻撃ステートが始まる前に呼び出される.
 	void AttackStart()
 	{
 		StateStartCommon();
 		status.attacking = true;
-		
+
 		// 敵の方向に振り向かせる.
 		Vector3 targetDirection = (attackTarget.position - transform.position).normalized;
 		SendMessage("SetDirection", targetDirection);
-		
+
 		// 移動を止める.
 		SendMessage("StopMove");
 	}
-	
+
 	// 攻撃中の処理.
 	void Attacking()
 	{
 		if (charaAnimation.IsAttacked())
 			ChangeState(State.Walking);
 	}
-	
+
 	void Died()
 	{
 		status.died = true;
 		gameRuleCtrl.GameOver();
+
+		// オーディオの再生.
+		deathSeAudio.Play ();
 	}
-	
+
 	void Damage(AttackArea.AttackInfo attackInfo)
 	{
+		GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity) as GameObject;
+		effect.transform.localPosition = transform.position + new Vector3(0.0f, 0.5f, 0.0f);
+		Destroy(effect, 0.3f);
+
 		status.HP -= attackInfo.attackPower;
 		if (status.HP <= 0)
 		{
@@ -144,7 +170,7 @@ public class PlayerCtrl : MonoBehaviour
 			ChangeState(State.Died);
 		}
 	}
-	
+
 	// ステートが始まる前にステータスを初期化する.
 	void StateStartCommon()
 	{
