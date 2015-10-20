9c965fb1fcad0a96626e3aee7e7028f037c24670
yhikishima <lallapalooza1120@gmail.com>
2015-10-21 08:35:02 +0900

    Add GameManager Script

---
+++ 13_RoguelikeGame/RoguelikeGame/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+  [Serializable]
+  public class Count {
+    public int minimum;
+    public int maximum;
+
+    public Count (int min, int max) {
+      minimum = min;
+      maximum = max;
+    }
+  }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
