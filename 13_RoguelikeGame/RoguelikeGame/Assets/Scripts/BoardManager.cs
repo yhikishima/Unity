8039b4d3383051582e3b98f8ab6e0c7a160d3acf
yhikishima <lallapalooza1120@gmail.com>
2015-10-24 15:05:02 +0900

    Add Script

--- 13_RoguelikeGame/RoguelikeGame/Assets/Scripts/BoardManager.cs
+++ 13_RoguelikeGame/RoguelikeGame/Assets/Scripts/BoardManager.cs
@@ -28,35 +28,60 @@ public class BoardManager : MonoBehaviour {
   public GameObject[] outerWallTiles;
 
   private Transform boardHolder;
-  private List <Vector> gridPositions = new List<Vector3>();
+  private List <Vector3> gridPositions = new List<Vector3>();
 
   void InitialiseList() {
     gridPositions.clear();
 
     for (int x = 1; x < columns -1; x++) {
-        for (int y = 1; y < rows -1; y++) {
-          gridPositions.Add(new Vector3(x,y,0f));
-        }
-    }
-  }
-
-  void BoardSetup() {
-    boardHolder = new GameObject ("Board").transform;
-
-    for (int x = -1; x < columns + 1; x++) {
-      for (int y = -1; y < rows; y++) {
-        GameObject toInstantiate = floorTiles[Random.Range (0, floorTiles.Length)];
+      for (int y = 1; y < rows -1; y++) {
+        gridPositions.Add(new Vector3(x,y,0f));
       }
     }
   }
 
-	// Use this for initialization
-	void Start () {
+  void BoardSetup () {
+    boardHolder = new GameObject ("Board").transform;
 
-	}
+    for (int x = -1; x < columns + 1; x++) {
+      for (int y = -1; y < rows +1; y++) {
+        GameObject toInstantiate = floorTiles[Random.Range (0, floorTiles.Length)];
+        if (x == -1 || x == colums || y == -1 || y == rows) {
+          toInstantiate = outerWaTiles[Random.Range (0,outerWallTitles.Length)];
+        }
 
-	// Update is called once per frame
-	void Update () {
+        GameObject instance = Instantiate(toInstantiate, new Vector3 (x,y,0f), Quaternion.identity) as GameObject;
 
-	}
+        instance.transform.SetParent(boardHolder);
+      }
+    }
+  }
+
+  Vector3 RandomPosition() {
+    int randomIndex = Random.Range(0, gridPositions.Count);
+    Vector3 randomPosition = gridPositions[randomIndex];
+    gridPositions.RemoveAt(randomIndex);
+    return randomPosition;
+  }
+
+  void LayoutObjectAtRandom(GameObject[] tileArray, int minimun, int maximu) {
+    int objectCount = Random.Range (minimun, maximu + 1);
+
+    for (int i = 0; i < objectCount; i++) {
+      Vector3 randomPosition = RandomPosition();
+      GameObject tileChoice = tileArray[Random.Range (0, tileArray.Length)];
+      Instantiate (tileChoice, randomPosition, Quaternion.identity);
+    }
+  }
+
+  public void SetupScene (int level) {
+    BoardSetup();
+    InitialiseList();
+    LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
+    LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
+    int enemyCount = (int)Mathf.Log(level, 2f);
+    LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
+    Instantiate (exit, new Vector3 (columns - 1, rows - 1, 0f), Quaternion.identity);
+
+  }
 }
