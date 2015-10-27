422879404561650fbb1d0def3149d7e3ea728206
yhikishima <lallapalooza1120@gmail.com>
2015-10-28 08:44:56 +0900

    Add Moving Object

---
+++ 13_RoguelikeGame/RoguelikeGame/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class MovingObject : MonoBehaviour {
+
+  public float moveTime = 0.1f;
+  public LayerMask blockingLayer;
+
+  private BoxCollider2D boxCollider;
+  private RigidBody2D rb2D;
+  private float inverseMoveTime;
+
+
+  protected virtual void Start () {
+    boxCollider = GetComponent<BoxCollider2D>();
+    rb2D = GetComponent<Rigidbody2D>();
+    inverseMoveTime = 1f / moveTime;
+  }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
