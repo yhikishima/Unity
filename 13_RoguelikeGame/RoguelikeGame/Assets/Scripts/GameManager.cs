cf81cdfd1bd860d485a41b9bf00d84d3b5873311
yhikishima <lallapalooza1120@gmail.com>
2015-11-09 09:01:47 +0900

    Create Enemy Attck Moving

--- 13_RoguelikeGame/RoguelikeGame/Assets/Scripts/GameManager.cs
+++ 13_RoguelikeGame/RoguelikeGame/Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
+  public float turnDelay = .1f;
 
   public static GameManager instance = null;
   public BoardManager boardScript;
@@ -9,28 +11,35 @@ public class GameManager : MonoBehaviour {
   [HideInInspector] public bool playersTurn = true;
 
   private int level = 3;
+	private List<Enemy> enemies;
+	private bool ememiesMoving;
 
-  void Awake () {
+	void Awake () {
 
-    if (instance == null)
-      instance = this;
-    else if (instance != this)
-      Destroy(gameObject);
-    boardScript = GetComponent<BoardManager>();
-    InitGame();
-  }
+		if (instance == null)
+		  instance = this;
+		else if (instance != this)
+		  Destroy(gameObject);
+		boardScript = GetComponent<BoardManager>();
+		InitGame();
+	}
 
 	// Use this for initialization
 	void InitGame () {
-    boardScript.SetupScene(level);
+	    boardScript.SetupScene(level);
+		enemies.Clear ();
 	}
 
-  public void GameOver() {
-    enabled = false;
-  }
+	public void GameOver() {
+		enabled = false;
+	}
 
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	IEnumerator MoveEnemies() {
+		enemiesMoving = true;
 	}
 }
