251bfe888d51c898e5f7a95518980528e5381a5a
yhikishima <lallapalooza1120@gmail.com>
2015-11-11 15:23:26 +0900

    Adjust GameManager

--- 13_RoguelikeGame/RoguelikeGame/Assets/Scripts/GameManager.cs
+++ 13_RoguelikeGame/RoguelikeGame/Assets/Scripts/GameManager.cs
@@ -2,63 +2,135 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class GameManager : MonoBehaviour {
-  public float turnDelay = .1f;
+using UnityEngine;
+using System.Collections;
 
-  public static GameManager instance = null;
-  public BoardManager boardScript;
-  public int playerFoodPoints = 100;
-  [HideInInspector] public bool playersTurn = true;
+using System.Collections.Generic;       //Allows us to use Lists. 
 
-  private int level = 3;
-	private List<Enemy> enemies;
-	private bool enemiesMoving;
-
-	void Awake () {
-
+public class GameManager : MonoBehaviour
+{
+	public float levelStartDelay = 2f;                      //Time to wait before starting level, in seconds.
+	public float turnDelay = 0.1f;                          //Delay between each Player turn.
+	public int playerFoodPoints = 100;                      //Starting value for Player food points.
+	public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+	[HideInInspector] public bool playersTurn = true;       //Boolean to check if it's players turn, hidden in inspector but public.
+	
+	
+	private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
+	private int level = 1;                                  //Current level number, expressed in game as "Day 1".
+	private List<Enemy> enemies;                          //List of all Enemy units, used to issue them move commands.
+	private bool enemiesMoving;                             //Boolean to check if enemies are moving.
+	
+	
+	
+	//Awake is always called before any Start functions
+	void Awake()
+	{
+		//Check if instance already exists
 		if (instance == null)
-		  instance = this;
+			
+			//if not, set instance to this
+			instance = this;
+		
+		//If instance already exists and it's not this:
 		else if (instance != this)
-		  Destroy(gameObject);
+			
+			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+			Destroy(gameObject);    
+		
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+		
+		//Assign enemies to a new List of Enemy objects.
+		enemies = new List<Enemy>();
+		
+		//Get a component reference to the attached BoardManager script
 		boardScript = GetComponent<BoardManager>();
+		
+		//Call the InitGame function to initialize the first level 
 		InitGame();
 	}
-
-	// Use this for initialization
-	void InitGame () {
-	    boardScript.SetupScene(level);
-		enemies.Clear ();
+	
+	//This is called each time a scene is loaded.
+	void OnLevelWasLoaded(int index)
+	{
+		//Add one to our level number.
+		level++;
+		//Call InitGame to initialize our level.
+		InitGame();
 	}
-
-	public void GameOver() {
-		enabled = false;
+	
+	//Initializes the game for each level.
+	void InitGame()
+	{
+		
+		//Clear any Enemy objects in our List to prepare for next level.
+		enemies.Clear();
+		
+		//Call the SetupScene function of the BoardManager script, pass it current level number.
+		boardScript.SetupScene(level);
+		
 	}
-
-	// Update is called once per frame
-	void Update () {
-		if (playersTurn || enemiesMoving)
+	
+	
+	//Update is called every frame.
+	void Update()
+	{
+		//Check that playersTurn or enemiesMoving or doingSetup are not currently true.
+		if(playersTurn || enemiesMoving)
+			
+			//If any of these are true, return and do not start MoveEnemies.
 			return;
-
+		
+		//Start moving enemies.
 		StartCoroutine (MoveEnemies ());
 	}
-
-	public void AddEnemyToList(Enemy script) {
-		enemies.Add (script);
+	
+	//Call this to add the passed in Enemy to the List of Enemy objects.
+	public void AddEnemyToList(Enemy script)
+	{
+		//Add Enemy to List enemies.
+		enemies.Add(script);
 	}
-
-	IEnumerator MoveEnemies() {
+	
+	
+	//GameOver is called when the player reaches 0 food points
+	public void GameOver()
+	{
+		
+		//Disable this GameManager.
+		enabled = false;
+	}
+	
+	//Coroutine to move enemies in sequence.
+	IEnumerator MoveEnemies()
+	{
+		//While enemiesMoving is true player is unable to move.
 		enemiesMoving = true;
-		yield return new WaitForSeconds (turnDelay);
-		if (enemies.Count == 0) {
+		
+		//Wait for turnDelay seconds, defaults to .1 (100 ms).
+		yield return new WaitForSeconds(turnDelay);
+		
+		//If there are no enemies spawned (IE in first level):
+		if (enemies.Count == 0) 
+		{
+			//Wait for turnDelay seconds between moves, replaces delay caused by enemies moving when there are none.
 			yield return new WaitForSeconds(turnDelay);
 		}
-
-		for (int i = 0; i < enemies.Count; i++) {
-			enemies[i].MoveEnemy();
+		
+		//Loop through List of Enemy objects.
+		for (int i = 0; i < enemies.Count; i++)
+		{
+			//Call the MoveEnemy function of Enemy at index i in the enemies List.
+			enemies[i].MoveEnemy ();
+			
+			//Wait for Enemy's moveTime before moving next Enemy, 
 			yield return new WaitForSeconds(enemies[i].moveTime);
 		}
-
+		//Once Enemies are done moving, set playersTurn to true so player can move.
 		playersTurn = true;
+		
+		//Enemies are done moving, set enemiesMoving to false.
 		enemiesMoving = false;
 	}
 }
