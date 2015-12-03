1b979d008e731e3124176c85937d8784134fe3f0
yhikishima <lallapalooza1120@gmail.com>
2015-12-03 09:22:30 +0900

    Add click event

--- 99_study_book/New Unity Project/Assets/scripts/KeyScript.cs
+++ 99_study_book/New Unity Project/Assets/scripts/KeyScript.cs
@@ -3,16 +3,24 @@ using System.Collections;
 
 public class KeyScript : MonoBehaviour {
 	Vector3 vec;
+	Vector3 stdSize;
+	Vector3 smlSize;
+	RaycastHit hit;
+	int counter = 0;
+	bool flg = false;
 
 	// Use this for initialization
 	void Start () {
-		vec = new Vector3 (0.1f, 0.1f, 0.1f); 	
+		vec = new Vector3 (0.1f, 0.1f, 0.1f);
+		stdSize = new Vector3 (1f, 1f, 1f);
+		smlSize = new Vector3 (0.5f, 0.5f, 0.5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		OperateKey ();
 		CubeRotate ();
+		MouseButtonDown ();
 	}
 
 	private void  OperateKey() {
@@ -36,6 +44,21 @@ public class KeyScript : MonoBehaviour {
 	private void CubeRotate() {
 		if (Input.GetMouseButton (0)) {
 			transform.Rotate(vec);
+		}
+	}
+
+	private void MouseButtonDown() {
+		if (flg) {
+			if (counter <= 0) {
+
+			}
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 pos = Input.mousePosition;
+			pos.z = 3.0f;
+			Vector3 newpos = Camera.main.ScreenToWorldPoint(pos);
+			transform.position = newpos;
 		}
 	}
 }
