8abb79ab845294cb3710085c9d2884dfd021e55e
yhikishima <lallapalooza1120@gmail.com>
2015-12-10 08:59:32 +0900

    Add Texture Script

--- 99_study_book/New Unity Project/Assets/scripts/TextureScript.cs
+++ 99_study_book/New Unity Project/Assets/scripts/TextureScript.cs
@@ -5,12 +5,15 @@ public class TextureScript : MonoBehaviour {
 	Texture texture1;
 	Renderer renderer;
 	Color color1;
+	float size = 1.0f;
 
 	// Use this for initialization
 	void Start () {
-		texture1 = (Texture)Resources.Load("MudRockNormals");
+		texture1 = (Texture)Resources.Load("MudRockyNormals");
 		renderer = GetComponent<Renderer> ();
 		color1 = renderer.material.color;
+		renderer.material.mainTexture = texture1;
+		renderer.material.color = Color.white;
 	}
 	
 	// Update is called once per frame
@@ -24,6 +27,16 @@ public class TextureScript : MonoBehaviour {
 			renderer.material.mainTexture = null;
 			renderer.material.color = color1;
 		}
-	
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			size += 0.01f;
+			renderer.material.SetTextureScale("_MainTex", new Vector2(size, size));
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			size -= 0.01f;
+			if (size < 0) {
+				size = 0f;
+			}
+			renderer.material.SetTextureScale("_MainTex", new Vector2(size, size));
+		}
 	}
 }
