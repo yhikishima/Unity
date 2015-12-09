71280802ca656ea88af6394d1b2857aeb9a54ffe
yhikishima <lallapalooza1120@gmail.com>
2015-12-09 09:05:43 +0900

    Add Texture Script

---
+++ 99_study_book/New Unity Project/Assets/TextureScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureScript : MonoBehaviour {
+	Texture texture1;
+	Renderer renderer;
+	Color color1;
+
+	// Use this for initialization
+	void Start () {
+		texture1 = (Texture)Resources.Load("MudRockNormals");
+		renderer = GetComponent<Renderer> ();
+		color1 = renderer.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (1f, 1f, 0.1f);
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			renderer.material.mainTexture = texture1;
+			renderer.material.color = Color.white;
+		}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			renderer.material.mainTexture = null;
+			renderer.material.color = color1;
+		}
+	
+	}
+}
