6d9667b17d01587448c7314b8953190af87c2b8b
yhikishima <lallapalooza1120@gmail.com>
2015-12-11 09:09:08 +0900

    Start Animation Study

---
+++ 99_study_book/New Unity Project/Assets/scripts/Animation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AnimatorClip clip = new AnimationClip ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
