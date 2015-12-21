9882ba9f6ed7c7485dee86925aab6b280261e27b
yhikishima <lallapalooza1120@gmail.com>
2015-12-21 09:08:41 +0900

    Once end Script

--- 99_study_book/New Unity Project/Assets/scripts/SmartballScript.cs
+++ 99_study_book/New Unity Project/Assets/scripts/SmartballScript.cs
@@ -18,22 +18,22 @@ public class SmartballScript : MonoBehaviour {
 			AnimationClip clip = new AnimationClip();
 			clip.legacy = true;
 			Keyframe[] keysX = new Keyframe[2];
-			keysX[0] = new Keyframe(0f, move.x, -3);
+			keysX[0] = new Keyframe(0f, move.x -3);
 			keysX[1] = new Keyframe(i + 1f, move.x -3);
 			AnimationCurve curveX = new AnimationCurve(keysX);
 			clip.SetCurve("", typeof(Transform), "localPosition.x", curveX);
 			clip.wrapMode = WrapMode.PingPong;
 			Keyframe[] keysY = new Keyframe[2];
 			keysY[0] = new Keyframe(0f, move.z);
-			keysY[1] = new Keyframe(i, 1f, move.y);
+			keysY[1] = new Keyframe(i + 1f, move.y);
 			AnimationCurve curveY = new AnimationCurve(keysY);
 			clip.SetCurve("", typeof(Transform), "localPosition.y", curveY);
 			Keyframe[] keysZ = new Keyframe[2];
 			keysZ[0] = new Keyframe(0f, move.z);
-			keysZ[1] = new Keyframe(i, 1f, move.z);
+			keysZ[1] = new Keyframe(i + 1f, move.z);
 			AnimationCurve curveZ = new AnimationCurve(keysZ);
 			clip.SetCurve("", typeof(Transform), "localPosition.z", curveZ);
-			cubes[i].GetComponent<Animation>().AddClip(clip, "clip1");
+			cubes[i].GetComponent<Animation>().AnimationClip(clip, "clip1");
 			cubes[i].GetComponent<Animation>().Play(clip, "clip1");
 
 
@@ -42,6 +42,32 @@ public class SmartballScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+		Renderer renderer = GetComponent<Renderer> ();
+
+		MoveCube ();
+
+		rigidbody.AddForce (0f, 0f, -1f);
+		if (Input.GetKey(KeyCode.Space)) {
+			power += 0.01f;
+			if (power > 1f) {
+				power = 1f;
+			}
+
+			renderer.material.color = new Color(1f, power, 0f);
+		}
+
+		if (Input.GetKeyUp(KeyCode.Space)) {
+			rigidbody.AddForce(new Vector3(0f,0f, power * 2500f));
+			power = 0f;
+			renderer.material.color = Color.red;
+		}
 	
+	}
+
+	void MoveCube () {
+		for (int i = 0; i<5; i++) {
+			cubes[i].transform.Rotate(new Vector3(0f,2.5f,0f));
+		}
 	}
 }
