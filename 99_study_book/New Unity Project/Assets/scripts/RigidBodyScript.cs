02198a49c7b5acc1977c855cb2062172b9afd7ab
yhikishima <lallapalooza1120@gmail.com>
2015-12-23 13:38:34 +0900

    New Scene and study rigidbody action

---
+++ 99_study_book/New Unity Project/Assets/scripts/RigidBodyScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigidBodyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject cube = GameObject.Find("Cube2");
+		Debug.Log(cube);
+
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+
+		cube.transform.Rotate (1f, -1f, -1f);
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			rigidbody.AddForce(new Vector3(-1f, 0, 1f)); // To left roll
+		}
+
+	}
+}
