0d984324ec3f48739393b09b45c7004866d355e5
yhikishima <lallapalooza1120@gmail.com>
2016-01-01 20:28:56 +0900

    create function

--- 14_Fushigino-dungeon-game/Fushigino-dungeon-game/Assets/Scripts/Layer2D.cs
+++ 14_Fushigino-dungeon-game/Fushigino-dungeon-game/Assets/Scripts/Layer2D.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class Layer2D : MonoBehaviour {
 
+	const int MAP_MAX_X = 34; // ステージの幅
+	const int MAP_MAX_Z = 34; // ステージの高さ
+	public GameObject block; // ブロックオブジェクト
+
+	int[,] mapNo = new int[MAP_MAX_Z, MAP_MAX_X];             // 34×34のマップ番号
+	GameObject[,] obj = new GameObject[MAP_MAX_Z, MAP_MAX_X]; // 34×34のオブジェクト
+
 	// Use this for initialization
 	void Start () {
-	
+		// 初めはブロックを全て埋める
+		for (int y = 0; y < MAP_MAX_Z; y++)
+		{
+			Debug.Log ("sssss");
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	void AllWall() {
+
 	}
 }
---
+++ 14_Fushigino-dungeon-game/README.md
@@ -0,0 +1,7 @@
+Unity Fushigino dungeon game
+==========
+不思議のダンジョンを作成する
+
+##参考
+- [ゲーム開発関連のメモがき自分用](http://gamexunity.blogspot.jp/2015/09/blog-post_61.html)
+- [とあるプログラマの備忘録](http://raharu0425.hatenablog.com/entry/20130131/1359601502)
