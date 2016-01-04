c7cd0bdc6c7cc3a04a8d2a2a5512d7a0b47b4b09
yhikishima <lallapalooza1120@gmail.com>
2016-01-04 22:11:01 +0900

    Add Halo script

--- 99_study_book/New Unity Project/Assets/scripts/ParticleScript.cs
+++ 99_study_book/New Unity Project/Assets/scripts/ParticleScript.cs
@@ -15,6 +15,49 @@ public class ParticleScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+		foreach(GameObject obj in cubes) {
+			obj.transform.Rotate(new Vector3(1f, 1f, 1f));
+		}
+		Vector3 v = transform.position;
+		v.y += 2;
+		v.z -= 7;
+		Camera.main.transform.position = v;
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			rigidbody.AddForce(new Vector3(-1f, 0, 0)); // To left roll
+		}
+		
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			rigidbody.AddForce (new Vector3(1f, 0, 0)); // TO right roll
+		}
+		
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			rigidbody.AddForce (new Vector3(0, 0, 1f)); // TO top roll
+		}
+		
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			rigidbody.AddForce (new Vector3(0, 0, -1f)); // TO bottom roll
+		}
+
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.name.StartsWith("Cube")) {
+			for(int i = 0; i < 4; i++) {
+				if (cubes[i] == collider.gameObject) {
+					ParticleSystem ps = gos[i].GetComponent<ParticleSystem>();
+					ps.Play ();
+					cubes[i].SetActive(false);
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider collider) {
+		if (collider.name == "Cylinder") {
+			Behaviour b = (Behaviour)collider.gameObject.GetComponent("Halo");
+			b.enabled = true;
+		}
 	}
 }
