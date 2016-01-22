49c25941d738bca11f2f03c1293ca749d53ab03a
yhikishima <lallapalooza1120@gmail.com>
2016-01-22 09:08:22 +0900

    Add Camerawork Script

---
+++ 99_unity-chan/Assets/UnityChan/Scripts/SampleUnityChanCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleUnityChanCamera : MonoBehaviour {
+	public GameObject target = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = target.transform.position;
+		position.y += 1f;
+		position.z -= 3f;
+		transform.position = position;
+	}
+}
