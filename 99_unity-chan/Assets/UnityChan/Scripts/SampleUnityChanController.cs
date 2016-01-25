583d3257dc661270e0e651495883507466346714
yhikishima <lallapalooza1120@gmail.com>
2016-01-25 09:32:32 +0900

    Add Unity chan script for Jump

--- 99_unity-chan/Assets/UnityChan/Scripts/SampleUnityChanController.cs
+++ 99_unity-chan/Assets/UnityChan/Scripts/SampleUnityChanController.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (Animator))]
 public class SampleUnityChanController : MonoBehaviour {
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
+		anim = GetComponent<Animator> ();
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		anim.SetFloat ("Speed", v);
+		anim.SetFloat ("Direction", h);
+		anim.SetBool ("jump", false);
+		Vector3 vector = new Vector3 (0, 0, v);
+		vector = transform.TransformDirection (vector) * 5f;
+		transform.localPosition += vector * Time.fixedDeltaTime;
+		transform.Rotate (0, h, 0);
+
+		if (Input.GetButtonDown("Jump")) {
+			GetComponent<Rigidbody> ().AddForce (Vector3.up);
+			anim.SetBool ("Jump", true);
+		}
 	}
 }
---
+++ 99_unity-chan/Assets/UnityChan/Scripts/SampleUnityChanTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleUnityChanTrigger : MonoBehaviour {
+	Color oldcolor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		oldcolor = collider.GetComponent<Renderer> ().material.color;
+		collider.GetComponent<Renderer> ().material.color = Color.magenta;
+
+	}
+
+	void OnTriggerExit(Collider collider) {
+		collider.GetComponent<Renderer> ().material.color = oldcolor;
+	}
+}
