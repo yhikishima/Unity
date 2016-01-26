d88098f1918f85ec0bd313511733ff030870f281
yhikishima <lallapalooza1120@gmail.com>
2016-01-26 09:01:05 +0900

    Add new Scene for GUI

---
+++ 99_study_book/New Unity Project/Assets/scripts/guiSceneScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class guiSceneScript : MonoBehaviour {
+	public Canvas canvas;
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		canvas.enabled = false;
+		text.text = "ready....";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+			canvas.enabled = true;
+		}
+	}
+
+	public void OnButtonClick () {
+		text.text = "ok!!";
+	}
+}
