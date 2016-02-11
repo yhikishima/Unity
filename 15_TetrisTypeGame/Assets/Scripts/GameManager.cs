4ba4c78efc77997dab37b832d28144499162ec00
yhikishima <lallapalooza1120@gmail.com>
2016-02-12 08:56:17 +0900

    Add GameManager Script for block

---
+++ 15_TetrisTypeGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public GameObject Block;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate(Block);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Instantiate(Block);
+	}
+}
