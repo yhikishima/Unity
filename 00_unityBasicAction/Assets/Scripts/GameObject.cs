3dd5dff82b30d7ccb2aa1d5028bad511b719635d
yhikishima <lallapalooza1120@gmail.com>
2016-03-31 09:05:00 +0900

    Add basic script

---
+++ 00_unityBasicAction/Assets/Scripts/GameObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// 3.1 オブジェクトの親子関係
+		GameObject div = new GameObject();
+		div.transform.name = "First Object";
+		GameObject div2 = new GameObject ();
+		div2.transform.name = "Second Object";
+		div.transform.parent = div2.transform;
+	
+	}
+}
