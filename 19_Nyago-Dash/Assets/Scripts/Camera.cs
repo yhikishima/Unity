47bae90edc02ce73315b262ffba7d34e1703c7ff
yhikishima <lallapalooza1120@gmail.com>
2016-04-09 16:00:40 +0900

    Nyagon-dash | robot add

---
+++ 19_Nyago-Dash/Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+	public Transform target;
+	private Vector3 targetOffset; // ターゲットとの相対座標
+
+	// Use this for initialization
+	void Start () {
+		FollowRobot ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 自分の座標にtargetの座標を代入する + デフォルトの相対座標
+		GetComponent<Transform>().position = target.position + targetOffset;
+	}
+
+	private void FollowRobot () {
+		//自分自身とtargetとの相対距離を求める
+		targetOffset = GetComponent<Transform>().position - target.position;
+	}
+}
---
+++ 19_Nyago-Dash/Assets/Scripts/Robot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Robot : MonoBehaviour {
+	static bool isGoal = false;
+
+	// Use this for initialization
+	void Start () {
+		setAction ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		walkForward ();
+		setAction ();
+	}
+
+	private void walkForward() {
+		if (!isGoal) {
+			transform.position = new Vector3(transform.position.x - 0.1f, 0f, transform.position.z);			
+		}
+	}
+
+	private void setAction() {
+		// PC向け、キー入力
+		if (Input.GetKey("up")) {
+//			transform.position = new Vector3(transform.position.x - 0.1f, 0f, transform.position.z);
+		} else if (Input.GetKey("right")) {
+			transform.position = new Vector3(transform.position.x, 0f, transform.position.z + 0.1f);
+		} else if (Input.GetKey("left")) {
+			transform.position = new Vector3(transform.position.x, 0f,  transform.position.z - 0.1f);
+		}
+	}
+}
