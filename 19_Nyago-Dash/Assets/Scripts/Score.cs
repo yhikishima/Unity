c09b6c2577e26bf08977e5bcd49dbeb0eddc577d
yhikishima <lallapalooza1120@gmail.com>
2016-04-21 09:01:17 +0900

    Nyago Dash | create start

--- 19_Nyago-Dash/Assets/Scripts/Score.cs
+++ 19_Nyago-Dash/Assets/Scripts/Score.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 using System.Collections;
 
 public class Score : MonoBehaviour {
-  int score = 0;
+  public int score = 0;
+  private string RANKING_PREF_KEY = "ranking";
+  private float[] ranking = new float[10];
+
   void Awake() {
-    score = PlayerPrefs.GetInt("Score");
+    score = PlayerPrefs.GetInt(RANKING_PREF_KEY, 0);
+    foreach(float val in ranking) {
+      Debug.Log(val);
+    }
   }
   void Update() {
     
   }
 
-  public void SetScore() {
-    PlayerPrefs.SetInt("Score", Score);
+  public int dealScore {
+    get {
+      return score;
+    }
+    set {
+      PlayerPrefs.SetInt("score", score);
+    }
+  }
+  
+  public void getRanking() {
+    // float[] _ranking = PlayerPrefs.GetString(RANKING_PREF_KEY);
+    // if (_ranking.length > 0) {
+      
+    // }
+  }
+  
+  public void setRanking(float newScore) {
+    // float tmp = 0f;
+    // var ranking = ranking;
+    // if (ranking) {
+
+    // } else {
+    //   ranking[0] = newScore;
+    // }
+    
+    // string[] rankingArray = ranking
+    // PlayerPrefs.SetString(RANKING_PREF_KEY, rankingArray);
+  }
+  
+  public void deleteScore() {
+    PlayerPrefs.DeleteKey(RANKING_PREF_KEY);
   }
 }
