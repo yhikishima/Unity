5c9abb9c4e533fc509e4a9d4f5d6e8a096f5f943
yhikishima <lallapalooza1120@gmail.com>
2016-06-08 09:10:01 +0900

    unity_rails_test | api取得

--- 80_unity_rails_test/Assets/Scripts/api/APIManager.cs
+++ 80_unity_rails_test/Assets/Scripts/api/APIManager.cs
@@ -1,14 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 using MiniJSON;
-using System;
+using System.Collections.Generic;
 
 public class APIManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	private string listUrl = "http://localhost:3000/lists";
 
+	// Use this for initialization
+	IEnumerator Start () {
+		WWW www = new WWW(listUrl);
+		yield return www;
+
+		Debug.Log("読み込み完了");
+
+		Debug.Log(www);
+
+		var jsonText = www.text;
+
+		// 文字列を json に合わせて構成された辞書に変換
+		var json = Json.Deserialize (jsonText) as Dictionary<string, string>;
+
+		Debug.Log(json);
+
+		// Debug.Log((Sting)jsonDict["message"]);
 	}
 
 	// Update is called once per frame
