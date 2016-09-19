a2eddba3f5a2f0e1cd74309e4e38ad50810ff63e
yhikishima <lallapalooza1120@gmail.com>
2016-09-19 22:37:15 +0900

    add first commit

---
+++ 23_unity_master_data/Assets/Datas/Master.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace UnityMaster
+{
+	public class master : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start ()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update ()
+		{
+
+		}
+	}
+}
+
